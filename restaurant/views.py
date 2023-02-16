98c542f4adfce4b2aef6192c3e1544ae195371ee
Артем Апенко <lbapart@MacBook-Pro.local>
2023-02-16 18:20:04 +0100

    added authentication

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
 
@@ -16,6 +17,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class = MenuSerializer
     
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
